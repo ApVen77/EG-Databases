8a18030fffa7a2c8c005a814f33044f7f69a0dd5
Ngoc Doan <ngocdoan241@gmail.com>
2021-03-23 18:34:38 -0600

    added routes in app.js

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Routes 
+
+var express = require('express');
+var expressJade = require('../');
+var serveStatic = require('serve-static');
+var path = require('path');
+var app = express();
+ 
+app.use(serveStatic(path.join(__dirname)));
+ 
+if (process.env.NODE_ENV !== 'production') {
+  var viewsDir = path.join(__dirname, 'views');
+  var namespace = 'jade';
+  app.get('/js/templates/*', expressJade(viewsDir, namespace));
+}
+ 
+app.listen(3000);
+
+
+
 //for jade(html) files in the views folder
 //res.render Used in Express to view and send HTML to the client 
 
